30d0d3f84614aef16b2367fa78f339a006e243c0
seguimiento-politico <kinsay.spam+github@gmail.com>
2023-07-29 17:09:03 +0100

    document view improved + back to top button fixed

--- assets/js/back_to_top.js
+++ assets/js/back_to_top.js
@@ -1,19 +1,27 @@
-//Get the button
-var mybutton = document.getElementById("back-to-top");
+// author: https://mdbootstrap.com/docs/standard/extended/back-to-top/
 
-// When the user scrolls down 1000px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+if (
+document.body.scrollTop > 500 ||
+document.documentElement.scrollTop > 500
+) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
 }
-
+}
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
 }
