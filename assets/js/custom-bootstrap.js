1a9d2c53b0f1b14e2357e235077075ad396d8024
seguimiento-politico <kinsay.spam+github@gmail.com>
2023-08-23 16:44:02 +0100

    improved document UI

--- assets/js/custom-bootstrap.js
+++ assets/js/custom-bootstrap.js
@@ -11,4 +11,19 @@ $(function() {
         $('html,body').animate({
         scrollTop: $card.offset().top - additionalOffset }, 100);
     });
+
+    $('#doc_content').on('shown.bs.collapse', function () {
+	
+        var panel = $(this).find('.in');
+        
+        $('html, body').animate({
+            scrollTop: panel.offset().top
+        }, 500);
+        
+    });
+    
 });
+
+
+
+
